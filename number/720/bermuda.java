a679a624f8d62f73b3a20d09e8c52989d780181e
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-01 04:37:13 -0700

    Update bermuda.java

--- number/720/bermuda.java
+++ number/720/bermuda.java
@@ -2,17 +2,17 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Path2D;
 public class BermudaTriangle {
     public static void main(String[] args) {
-        // 定义百慕大三角的顶点
         Point2D.Double vertex1 = new Point2D.Double(25.774, 77.006); // 佛罗里达州 florida
         Point2D.Double vertex2 = new Point2D.Double(18.466, 66.118); // 波多黎各 PuertoRico
         Point2D.Double vertex3 = new Point2D.Double(31.424, 64.151); // 百慕大 bermuda
-        // 创建一个Path2D对象来表示百慕大三角的边界
         Path2D.Double path = new Path2D.Double();
         path.moveTo(vertex1.getX(), vertex1.getY());
         path.lineTo(vertex2.getX(), vertex2.getY());
         path.lineTo(vertex3.getX(), vertex3.getY());
         path.closePath();
-        // 输出百慕大三角的边界
         System.out.println("Bermuda Triangle boundary points: " + path);
     }
 }
+// 定义百慕大三角的顶点
+// 创建一个Path2D对象来表示百慕大三角的边界
+// 输出百慕大三角的边界
