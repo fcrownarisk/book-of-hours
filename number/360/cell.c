1be9910f2f8071611811b7d42bebc0303ce9f1d2
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-01 14:22:50 +0800

    Update cell.c

--- number/360/cell.c
+++ number/360/cell.c
@@ -1,25 +1,28 @@
 #include "stdatomic.h"
+#define Ribosome int8_t
+#define Lysosome int16_t
+#define Centrosome int32_t
+#define handsome int64_t
 void cell(){ 
-char Cell_Membrane = "'inner' + 'outer'+ 'middle'"
-char Cell_Plasma = __EDG__ + __EDG_PTRDIFF_TYPE__ + __EDG_SIZE_TYPE__
-char Cytoplasmic_Matrix = memory_order_relaxed % memory_order_release
-char Endoplasmic_Reticulum 
-void function openNC(){
-char Nucleus =
-char Chromatin =
+char Cell_Membrane = "'inner' + 'outer'+ 'middle'";
+char Cell_Plasma = "__EDG__ + __EDG_PTRDIFF_TYPE__ + __EDG_SIZE_TYPE__";
+char Cytoplasmic_Matrix = "memory_order_relaxed % memory_order_release";
+
+char Endoplasmic_Reticulum = "";
+char Mitochondria = "";
+char Golgi_Apparatus = "";
+char GolhiBody = "";
+  
+void openNC(){
+char Nucleus = "NNN,NnNnNn,nnn";
+char Chromatin = "Cc CcCc CcCcCc";
 }
-char Mitochondria =
-
-
-char Golgi_Apparatus =
-char GolhiBody =
-
-void function openCV(){ 
-char Cholorolast = ""
-char Vacuole = ""
+void openCV(){ 
+char Cholorolast = "Colo Cola";
+char Vacuole = "V vv vv V";
 }
-int8_t Ribosome = 
-int16_t Lysosome =
-int32_t Centrosome =
-int64_t handsome = Ribosome + Lysosome + Centrosome
+char Ribosome = "Int8Array + Int16Array + Int32Array";
+char Lysosome = "Float16Array + Float32Array";
+char Centrosome = "Unit8Array + UnitClamped8Array + Unit16Array + Unit32Array";
+char handsome = "Ribosome + Lysosome + Centrosome";
 }
