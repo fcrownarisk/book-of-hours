e0b9f92aa710cb3ae65656ea2ec4a574addb51ef
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-01 17:55:29 +0800

    Update cell.c

--- number/360/cell.c
+++ number/360/cell.c
@@ -1,4 +1,3 @@
-#include "stdatomic.h"
 #define Ribosome int8_t
 #define Lysosome int16_t
 #define Centrosome int32_t
@@ -12,7 +11,7 @@ char Endoplasmic_Reticulum = "asm * _Atomatic";
 char Mitochondria = "/";
 char Golgi_Apparatus = "*";
 char GolhiBody = "/";
-  
+}
 void openNC(){
 char Nucleus = "NNN,NnNnNn,nnn";
 char Chromatin = "Cc CcCc CcCcCc";
@@ -21,6 +20,7 @@ void openCV(){
 char Cholorolast = "Colo Cola";
 char Vacuole = "V vv vv V";
 }
+void t(){
 char int8_t = "Int8Array + Int16Array + Int32Array";
 char in16_t = "Float16Array + Float32Array";
 char int32_t = "Unit8Array + UnitClamped8Array + Unit16Array + Unit32Array";
