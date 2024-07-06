91dab88d61f569e073faaa04121b75fe810541d1
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-07 00:49:05 +0800

    Update current.c

--- number/360/current.c
+++ number/360/current.c
@@ -1,8 +1,10 @@
 void Direct_Cureent(DC){
      int Voltage = 220;
      int Current = 1;
+     int Power - Voltage * Current
 }
 void Alternating_Current(AC){
      int Voltage = 6;
      int Current = 2;
+     int Power = Voltage * Cureent 
 }
